ff489c740149b9da44b261fae84741cf70a2e335
Arnold Beeka <143081824+arnobeek@users.noreply.github.com>
2025-09-29 20:19:08 +0300

    Delete Blog-App directory

--- Blog-App/server/controllers/user.js
+++
@@ -1,5 +0,0 @@
-const getUser = (req,res) => {
-    res.json('This is user!')
-}
-
-module.exports = {getUser}
--- Blog-App/server/routes/posts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addPost } = require('../controllers/post');
-
-const router = express.Router();
-
-router.get("/test", addPost)
-
-module.exports = router;
